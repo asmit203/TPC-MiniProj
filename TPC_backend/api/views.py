29c691db8b69b637da09620827c22ae61a8e23dd
Divit <divitajmera15@gmail.com>
2023-04-10 18:03:31 +0530

    added rest framework dependency

---
+++ TPC_backend/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+import rest_framework.status as status
+from rest_framework.decorators import api_view
+from users.models import Student, Alumni, Company
+from .serializers import StudentSerializer
+
+@api_view(['POST'])
+def login(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response({'message': 'Error! Could not login'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data)
