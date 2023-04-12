08391f6f7ef0183888a9b7456d493d5b57af2b7a
Asmit Ganguly <asmit.ganguly@gmail.com>
2023-04-13 04:38:13 +0530

    admin

--- TPC_backend/users/admin.py
+++ TPC_backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from users.models import Student, Company, Alumni, Credits
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Student)
+admin.site.register(Alumni)
+admin.site.register(Credits)
