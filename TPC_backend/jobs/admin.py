3d1479b1bbc412d946528620f0c639a9cce311a0
Arkadeep <acharyarka17@gmail.com>
2023-04-13 04:03:49 +0530

    admin

--- TPC_backend/jobs/admin.py
+++ TPC_backend/jobs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from jobs.models import job,applied
+# Register your models here..
+
+class jobAdmin(admin.ModelAdmin):
+    pass
+      
+    
+class appliedAdmin(admin.ModelAdmin):
+    pass
+    
+
+admin.site.register(job,jobAdmin)
+admin.site.register(applied,appliedAdmin)
