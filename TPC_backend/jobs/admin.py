4412687f24829a6e7f22401e0b02687740778a75
Arkadeep <acharyarka17@gmail.com>
2023-04-13 04:20:14 +0530

    admin of job

--- TPC_backend/jobs/admin.py
+++ TPC_backend/jobs/admin.py
@@ -6,12 +6,12 @@ from jobs.models import job,applied
 # Register your models here..
 
 class jobAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in job._meta.get_fields()]
+    list_display = ['cid','jid','jobTitle','jobDesc']
 
     
     
 class appliedAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in applied._meta.get_fields()]
+    list_display = ['jid','roll_no','status','sid']
 
 admin.site.register(job,jobAdmin)
 admin.site.register(applied,appliedAdmin)
